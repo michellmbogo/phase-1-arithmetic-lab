8c9e3fb2e5b967fc577fb0003cdaaec95e74d5df
Michell Wambui Mbogo <mishie254@gmail.com>
2024-11-29 23:24:16 +0600

    Arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 10;  
+
+const mod = num3 % num4;
+
+const numbers = [1,5,20,12,9,] ;
+const max = Math.max(...numbers)
